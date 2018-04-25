f07d3052f2886f6dd2de3de508948c18b3757c2c
Bartosz Pogoda <bartek.pogoda1@gmail.com>
2018-04-25 15:29:09 +0200

    byc moze zadziala

--- vga_pixel.vhd
+++ vga_pixel.vhd
@@ -35,6 +35,10 @@ entity vga_pixel is
         h_counter: in std_logic_vector(10 downto 0);
         v_counter: in std_logic_vector(10 downto 0);
         vidon : in std_logic;
+        
+        player_x : in std_logic_vector(10 downto 0);
+        player_y : in std_logic_vector(10 downto 0);
+        player_action : in std_logic;
 
         red_out: out std_logic;
         green_out: out std_logic;
@@ -43,7 +47,7 @@ entity vga_pixel is
 end vga_pixel;
 
 architecture Behavioral of vga_pixel is
-
+    constant player_size : std_logic_vector(10 downto 0) := 30;
 begin
 
  draw_display_area : process(vidon, h_counter, v_counter)
@@ -53,13 +57,25 @@ begin
         blue_out <= '0';
 
         if vidon = '1' then
-            if h_counter > 100 and h_counter < 200 then
-               if v_counter > 100 and v_counter < 200 then
+            -- if h_counter > 100 and h_counter < 200 then
+            --    if v_counter > 100 and v_counter < 200 then
              
-                  green_out <= '1';
-               end if;
-            end if;
+            --       green_out <= '1';
+            --    end if;
+            -- end if;
         
+            if h_counter > player_x and h_counter < player_x + player_size then
+                if v_counter > player_y and v_counter < player_y + player_size then
+                    
+                    if player_action = '1' then
+                        red_out <= '1';
+                    else
+                        green_out <= '1';
+                    end if;
+
+                end if;
+            end if;
+
         end if;
     end process draw_display_area;
 
